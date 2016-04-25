7ba25ed474bdc4239e5bae24cbc0c83243990d29
Nancy <nansimalgapo@gmail.com>
2016-04-25 11:49:57 +0800

    done web

--- order/models.py
+++ order/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-from pygments.lexers import get_all_lexers
-
-LEXERS = [item for item in get_all_lexers() if item[1]]
 
 # Create your models here.
 class Customer(models.Model):
